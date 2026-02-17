0540f563db2d6b30d98689a2a7981405f7227391
ACastilleja <castillejaarturo@gmail.com>
2026-02-17 18:01:34 -0500

    added eventListener to messageForm

--- js/index.js
+++ js/index.js
@@ -33,7 +33,21 @@ for (var i=0; i<skills.length;i++){
     skillsList.appendChild(skill);
 }
 
+// Handling Message Form Submit
+var messageForm = document.getElementsByName('leave_message')[0];
 
+messageForm.addEventListener('submit',(event)=>{
+
+    event.preventDefault();
+
+    var userName = event.target.usersName.value;
+    var userEmail = event.target.usersEmail.value;
+    var userMessage = event.target.usersMessage.value;
+
+    console.log('Username:', userName);
+    console.log('Email:',userEmail);
+    console.log('Message:',userMessage);
+});
 
 
 
